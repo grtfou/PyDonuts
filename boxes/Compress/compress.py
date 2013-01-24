3df41fc3930e9504ec4c07db769a0ab257b02838
grtfou <grtfou@gmail.com>
2013-01-24 19:09:32 +0800

    Added the compress program.

---
+++ boxes/Compress/compress.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  @first_date    20110902
+#  @date          20130124
+#  @author        Chih-Yuan Chen
+#  @brief         This program can copy, packing files(directions) to another target place.
+
+#from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+import os
+import datetime
+import time
+
+import config
+
+SOURCE_PATH = os.path.abspath(config.SOURCE_PATH)
+SOURCE_DIR = config.SOURCE_DIR
+
+TARGET_PATH = os.path.abspath(config.TARGET_PATH)
+TARGET_DIR = config.TARGET_DIR
+
+COMPRESSED_MAIN = os.path.abspath(config.COMPRESSED_MAIN)
+DEFAULT_OUTPUT = config.DEFAULT_OUTPUT
+
+def compressed(input, output, type):
+    type = type.lower()
+    if type == 'gzip':
+        import tarfile
+
+        tar = tarfile.open("%s.tar.gz" % output, "w:gz")
+        for source in input:
+            tar.add(source, arcname=source.split(os.sep)[-1])
+
+        tar.close()
+    elif type == '7zip':
+        cmd = "{0} \"{1}\" {2}".format(COMPRESSED_MAIN, output, " ".join(input))
+        os.system(cmd)
+    else:
+        print("No found this compressed type.")
+
+def main(type, output):
+    today = datetime.datetime.now().strftime('%y-%m-%d')
+
+    for output_dir in TARGET_DIR:
+        append_path = []
+        for backup_folder in SOURCE_DIR:
+            input = os.path.join(SOURCE_PATH, backup_folder)
+            append_path.append(input)
+
+        output_path = os.path.join(TARGET_PATH, output_dir,
+                                   "%s(%s)" % (output, today))
+
+        compressed(append_path, output_path, type)
+
+        time.sleep(2)
+
+if __name__ == '__main__':
+    """
+    Get options
+    """
+    if len(sys.argv) > 2:
+        compression_output = sys.argv[2]
+    else:
+        compression_output = DEFAULT_OUTPUT
+
+    if len(sys.argv) > 1:
+        compression_type = sys.argv[1]
+    else:
+        compression_type = '7zip'
+
+    main(compression_type, compression_output)
+
+
---
+++ boxes/Compress/config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  @first_date    20130124
+#  @date
+#  @author        Chih-Yuan Chen
+#  @brief         Settings and configuration for Compress.
+
+# destination path
+# ex. 'D:\100-Dropbox\Dropbox\Public' and 'D:\100-Dropbox\Dropbox\Sharing'
+TARGET_PATH = r"D:\100-Dropbox\Dropbox"
+TARGET_DIR = ('Public', 'Sharing')
+
+# source path
+# ex. 'D:\\99_SystemTools\2_Design\AppServ\www\mywiki\data\page'..
+SOURCE_PATH = r"D:\99_SystemTools\2_Design\AppServ\www\mywiki\data"
+SOURCE_DIR = ('pages', 'media')
+
+# compress setting
+COMPRESSED_MAIN = r"D:\99_SystemTools\7-Zip\7z.exe a -t7z"
+DEFAULT_OUTPUT = 'pages'
