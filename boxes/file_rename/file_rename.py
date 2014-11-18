d468abf0c59d4c33653513bd5b4c871cc62154c8
grtfou <lightgdi@gmail.com>
2014-11-18 18:47:58 +0800

    Fix: Rename fail by path #1

--- boxes/file_rename/file_rename.py
+++ boxes/file_rename/file_rename.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #  @first_date    20120821
-#  @date          20141107
+#  @date          20141118 - Fixing bugs
 """
 Search file to rename new filename
 """
@@ -12,11 +12,18 @@ from __future__ import division
 import os
 import re
 
-class FastRename:
+class FastRename(object):
+    '''
+    File rename for regex
+    '''
+
     def __init__(self):
         pass
 
     def __list_files(self, is_include_sub=False):
+        '''
+        Visit file(folder) path by regex
+        '''
         search_file_list = []
         for dirname, _, filenames in os.walk(os.getcwd()):
             for filename in filenames:
@@ -28,49 +35,68 @@ class FastRename:
 
         return search_file_list
 
-    def rename(self, search_exp, replace_exp, is_include_sub=False, is_prod=True, search_type="xml"):
-        work_file_list = []
+    def rename(self, search_exp, replace_exp, **kwargs):
+        '''
+        Main function for rename the file
+        '''
+        is_prod = False
+        is_include_sub = False
+        search_type = "xml"
 
+        for name, value in kwargs.items():
+            if name == 'is_prod':
+                is_prod = value
+            elif name == 'is_include_sub':
+                is_include_sub = value
+            elif name == 'search_type':
+                search_type = value
+
+        ### travel path ###
         for work_path in self.__list_files(is_include_sub):
-            find_file_path = re.search(r".*\\({})\.{}$".format(search_exp, search_type),
-                                       work_path)
+            # old file name and path
+            dir_path = '{}{}'.format(os.sep.join(work_path.split(os.sep)[:-1]), os.sep)
+            old_filename = work_path.split(os.sep)[-1]
+
+            # Search rule
+            find_rule = r"({})\.{}$".format(search_exp, search_type)
+            find_file_path = re.search(find_rule, old_filename)
 
             if find_file_path:
-                print("Old Path Name: {}".format(work_path))
+                print("Old Path: {}".format(work_path))
 
-                new_name_set = re.search(r'(.*\\){}'.format(replace_exp), work_path)
+                # Checking filename for replace new name
+                new_name_set = re.search(replace_exp, find_file_path.group(1))
 
-                prefix_name = new_name_set.group(1)
-                suffix_name = new_name_set.group(2)
+                if new_name_set:
+                    new_path = '{}{}.{}'.format(dir_path, new_name_set.group(1), search_type)
+                    new_path_no_ext = '{}{}'.format(dir_path, new_name_set.group(1))
 
-                if new_name_set and prefix_name and suffix_name:
-                    new_file_name = '{}{}.jpg'.format(prefix_name, suffix_name)
-                    print("New Path Name: {}".format(new_file_name))
+                    # Avoid replace the same name file
+                    if os.path.exists(new_path):
+                        new_path_no_ext += "_1"
+                        new_path = '{}.{}'.format(new_path_no_ext, search_type)
+
+                    print("New Path: {}".format(new_path))
 
                     # Rename
                     if is_prod:
-                        os.rename(work_path, new_file_name)
+                        os.rename(work_path, new_path)
 
                 print("=" * 20)
 
-                work_file_list.append(work_path)
-
-        return work_file_list
-
 ### Main ###
 if __name__ == '__main__':
-    my_script = FastRename()
+    MAIN_SCRIPT = FastRename()
 
     ### Arguments by search ###
-    search_exp = r'.*'   # Search all files
-
-    replace_exp = r'[^0-9]*(\d*)'  # filename use first number only
-    # replace_exp = r'([\w ]*)-.*'   # before dash only
-    # replace_exp = r'.*?-([-\w ]*)' # after dash only
+    SEARCH_RULE = r'.*'                 # Search all files
+    # REPLACE_RULE = r'[^0-9]*(\d*)'    # filename use first number only
+    REPLACE_RULE = r'([\w\s]*)-.*'        # before dash only
+    # REPLACE_RULE = r'.*?-([-\w\s]*)'    # after dash only
     ###-arguments ###
 
-    my_script.rename(search_exp,
-                     replace_exp,
-                     is_include_sub=False,
-                     is_prod=False,
-                     search_type="jpg")
+    MAIN_SCRIPT.rename(SEARCH_RULE,
+                       REPLACE_RULE,
+                       is_include_sub=True,
+                       is_prod=False,
+                       search_type="jpg")
