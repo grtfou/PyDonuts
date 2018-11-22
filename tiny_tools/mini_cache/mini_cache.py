326fb9052b22971c0cb720e6bdf568746508d6c6
quokka <1241272+grtfou@users.noreply.github.com>
2018-11-22 16:06:55 +0800

    Fix bug and enhance debug mode

--- tiny_tools/mini_cache/mini_cache.py
+++ tiny_tools/mini_cache/mini_cache.py
@@ -1,36 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#  @first_date    20140807
-#  @date          20140811
-#  @version       1.0
 """
 mini cache
+  @first_date    140807
+  @date          181122
+  @TODO:         record limit (the oldest record will be replaced)
 """
 import inspect
 import time
 
-### limit the memory ###
-''' 限制cache最多用多少記憶體
-    目前下面的程式是 限制全部程式(含import的程式)，所以不好用
-    1048576L = 2的20次方 = 1048516 bytes = 1048.516K = 1 MB
 
-# import resource
-# megs = 5
-# resource.setrlimit(resource.RLIMIT_AS, (megs * 1048576L, -1L))
-# print("usage stats", "=>", resource.getrusage(resource.RUSAGE_SELF))
-#-
-'''
-
-# key: program + function
-# value: stored time
-#        function result (by function return)
+"""
+  * key: program filename + function name
+  * value: created time, function result
+"""
 __cache = {}
 
-##
-# TODO: 1. 優先序 (太久沒被讀的cache被替換掉)
-#          或者 先換掉最舊的cache (最多記多少筆cache)
-#       2. 轉換: 可以改設成存成檔案(I/O)
-def mini_cache(expired_time=3600, stored_engine='cache'):
+
+def mini_cache(expired_time=3600, debug=False):
     """
     Args:
         expired_time: (int) second of expired function result
@@ -42,7 +27,7 @@ def mini_cache(expired_time=3600, stored_engine='cache'):
             myfunc: (function object) main execution function
         """
         # program path for key of cache naming
-        target_path = inspect.getsourcefile(myfunc)
+        filename = inspect.getsourcefile(myfunc)
 
         def __set_cache(*args, **kwargs):
             """
@@ -50,18 +35,25 @@ def mini_cache(expired_time=3600, stored_engine='cache'):
                 *args, **kwargs: main execution function arguments
             """
             global __cache
-            key = "{},{}:{},{}".format(target_path, myfunc.func_name,
+            key = "{},{}:{},{}".format(filename, myfunc.__name__,
                                        args, kwargs)
             if key in __cache.keys():
-                print('find')
+                if debug:
+                    print('find it (in memory)')
                 now = time.time()
-                if (expired_time > 0) and (now - __cache[key][0] < expired_time):
-                    return __cache[key][1]
+                if (expired_time > 0) and (
+                   now - __cache[key][0] < expired_time):
 
-            __cache[key] = [time.time(),
+                    return __cache[key][1]
+            else:
+                if debug:
+                    print('not found')
+
+            __cache[key] = [int(time.time()),
                             myfunc(*args, **kwargs)]
 
-            print("cache: {}".format(str(__cache)))
+            if debug:
+                print("cache: {}".format(str(__cache)))
             return __cache[key][1]
 
         return __set_cache
--- tiny_tools/mini_cache/mini_main.py
+++ tiny_tools/mini_cache/mini_main.py
@@ -1,11 +1,14 @@
 import time
+import random
+
 from mini_cache import mini_cache
 
-@mini_cache(10)
-def main(x, y):
+
+@mini_cache(10, debug=False)
+def add(x, y):
     time.sleep(2)
     return x + y
 
-for i in range(1, 10):
-    print("Run {}: {}".format(str(i), main(1, 2)))
 
+for i in range(1, 10):
+    print("Run {}: result {}".format(i, add(1, random.randint(1, 3))))
